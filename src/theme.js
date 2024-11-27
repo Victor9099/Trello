2908a306b57d943387948bcc2fd02e76b7140fd4
D VIKTOR <Victor9099>
2024-11-27 17:30:58 +0700

    start 1

--- src/theme.js
+++ src/theme.js
@@ -2,11 +2,15 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: {
-          main: '#ff5252'
+          main: '#52c2ff21'
         }
       }
     },
